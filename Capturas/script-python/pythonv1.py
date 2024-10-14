4cb4453b4ef4465c7efdf8a05b08746552cd3fb8
JhonatanFrancozo <jhonatan.caldas@sptech.school>
2024-10-13 21:13:31 -0300

    Correção de personalização da main e alteração no script python.

--- Capturas/script-python/pythonv1.py
+++ Capturas/script-python/pythonv1.py
@@ -1,56 +1,56 @@
 # Imports de módulos externos necessários para manipular tempo,capturar dados e conectar com o BD MySQL
 import time
 import psutil as ps
-# from mysql.connector import connect, Error
+from mysql.connector import connect, Error
 
 def main(): # Função principal, fiz assim por questão de organização
-    imprimeMenu("Olá, Bem vindo(a)! Aqui está a captura de dados em tempo real de: Uso de CPU, Uso de RAM, Uso de Disco e Conexões Abertas em Rede!")
-    # escolha = input("Você deseja que os dados capturados sejam inseridos no Banco de Dados (S/N)? ")
-    # habilitarInsercao = False
+    imprimeMenu("Olá, Bem vindo(a)! Aqui está a captura de dados em tempo real de: Uso de CPU, Uso de RAM, Uso de Disco, Conexões Abertas em Rede, e Tempo de atividade!")
+    escolha = input("Você deseja que os dados capturados sejam inseridos no Banco de Dados (S/N)? ")
+    habilitarInsercao = False
     time.sleep(2.5)
     print("\n------------------ USO DE HARDWARE ------------------\n")
-    print(formataLinha('CPU (%)', 'RAM (%)', 'Uso de disco(%)'))
-    print('-' * 53)
+    print(formataLinha('CPU (%)', 'RAM (%)', 'DISCOLIVRE (GB)'))
+    print('-' * 45)
 
-    # if(escolha.lower() == "s"):
-    #     habilitarInsercao = True
+    if(escolha.lower() == "s"):
+        habilitarInsercao = True
 
     while True:
         # Coleta e exibe os dados do sistema
         dados = obterDadosSistema()
         # Caso o usuário queira, insere no banco de dados aqui:
-        # if (habilitarInsercao):
-        #     query = montaQuery(*dados)
-        #     executarQuery(query)
+        if (habilitarInsercao):
+            query = montaQuery(*dados)
+            executarQuery(query)
         # Imprime os dados na tabela
         print(formataLinha(*dados)) # Utilizando o "*" como forma de "expandir" a tupla em dados separados, pois se passasse sem isso, ficaria apenas 1 tupla inteira como parâmetro.
         # Aguarda 2 segundos antes de atualizar
         time.sleep(2)
 
-# def executarQuery(script): # Função responsável por inserir os dados no banco, recebe uma query SQL qualquer como parâmetro e a executa, usando as credenciais específicas
-#     config = {
-#       'user': 'root',
-#       'password': '2205',
-#       'host': 'localhost',
-#       'database': 'Python'
-#     }
-#
-#     try:
-#       db = connect(**config)
-#       if db.is_connected():
-#         db_info = db.get_server_info()
-#         # print('Connected to MySQL server version -', db_info)
-#
-#         with db.cursor() as cursor:
-#           cursor.execute(script)
-#           db.commit()  # Confirma a transação no banco de dados
-#           # print("Dados inseridos com sucesso!")
-#
-#         cursor.close()
-#         db.close()
-#
-#     except Error as e:
-#       print('Erro ao conectar com o MySQL -', e)
+def executarQuery(script): # Função responsável por inserir os dados no banco, recebe uma query SQL qualquer como parâmetro e a executa, usando as credenciais específicas
+    config = {
+      'user': 'root',
+      'password': '2205',
+      'host': 'localhost',
+      'database': 'logGuard'
+    }
+
+    try:
+      db = connect(**config)
+      if db.is_connected():
+        db_info = db.get_server_info()
+        # print('Connected to MySQL server version -', db_info)
+
+        with db.cursor() as cursor:
+          cursor.execute(script)
+          db.commit()  # Confirma a transação no banco de dados
+          # print("Dados inseridos com sucesso!")
+
+        cursor.close()
+        db.close()
+
+    except Error as e:
+      print('Erro ao conectar com o MySQL -', e)
 
 def imprimeMenu(texto): # Função responsável por imprimir o menu inicial, contém o nosso nome em ASCII ART, recebe os parâmetros de: texto a ser impresso e tempo a esperar após imprimir
     print(""" 
@@ -84,16 +84,22 @@ def obterDadosSistema(): # Aqui, colho os dados por componente separadamente:
 
     # Uso de Disco
     usoDisco = ps.disk_usage('/')
-    usadoDisco = usoDisco.used / (1024 ** 3)  # Convertendo de bytes para GB
+    #usadoDisco = usoDisco.used / (1024 ** 3)  # Convertendo de bytes para GB
     livreDisco = usoDisco.free / (1024 ** 3)  # Convertendo de bytes para GB
+    DiscoLivre = round(livreDisco,1)
 
     # Conexões Abertas em Rede
-    conexoesRede = len(ps.net_connections()) # Length (Largura) da lista de conexões com internet
+    #conexoesRede = len(ps.net_connections()) # Length (Largura) da lista de conexões com internet
 
-    return (porcentagemCpu, porcentagemMemoria, usadoDisco) # Retorno da função, a tupla com todos os dados em ordem.
+    # Tempo de Atividade
+    #uptime = ps.boot_time() # ps.boot_time() -> Retorna o timestamp desde a época, que é 01/01/1970, 00:00:00 em segundos
+    #tempoAtividade = time.time() - uptime # time.time() -> Retorna o tempo atual em segundos desde a época
+    #tempoAtividadeStr = time.strftime("%H:%M:%S", time.gmtime(tempoAtividade)) # Formata o tempo, no formato HH:MM:SS
 
-# def montaQuery(*dados):
-#     return "INSERT INTO DadoCapturado (`CPU%`, `RAM%`, `ConexoesRede`, `TempoAtividade`) VALUES ('{}', '{}', '{}')".format(*dados)
+    return (porcentagemCpu, porcentagemMemoria, DiscoLivre) # Retorno da função, a tupla com todos os dados em ordem.
+
+def montaQuery(*dados):
+    return "INSERT INTO DadoCapturado (`CPU%`, `RAM%`, `DISCOLIVRE%`) VALUES ('{}', '{}', '{}')".format(*dados)
 
 # Como quis rodar o arquivo pela função "main()", preciso fazer isso:
 # A variável "__name__" é uma variável que tem seu valor atribuído automaticamente pelo Python.
