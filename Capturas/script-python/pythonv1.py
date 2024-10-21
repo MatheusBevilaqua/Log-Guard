67a581740c78f754ad56faf0b3f542dc0243baa8
fabriciogomes86820605 <fabricio.bonacelli@sptech.school>
2024-10-20 21:33:48 -0300

    Python com inserção de dados montada

--- Capturas/script-python/pythonv1.py
+++ Capturas/script-python/pythonv1.py
@@ -2,62 +2,85 @@
 import time
 import psutil as ps
 from mysql.connector import connect, Error
-from mysql.connector import connect, Error
+from datetime import datetime
+
 
 def main(): # Função principal, fiz assim por questão de organização
     imprimeMenu("Olá, Bem vindo(a)! Aqui está a captura de dados em tempo real de: Uso de CPU, Uso de RAM, Uso de Disco e Conexões Abertas em Rede!")
     # escolha = input("Você deseja que os dados capturados sejam inseridos no Banco de Dados (S/N)? ")
     habilitarInsercao = True
     time.sleep(2.5)
-    print("\n------------------ USO DE HARDWARE ------------------\n")
-    print(formataLinha('CPU (%)', 'RAM (%)', 'DISCOLIVRE (GB)'))
-    print('-' * 45)
 
-    enderecoMAC = obtemMAC()
-    insertMACQuery = montaQueryMAC(enderecoMAC)
-    executarQuery(insertMACQuery)
+    psutilMAC = ps.net_if_addrs()
+    wifi = psutilMAC['Wi-Fi']
+    enderecoMAC = wifi[0].address
+    print('Endereço MAC capturado pelo psutil',enderecoMAC)
 
-    while True:
+
+    buscaMACQuery = buscaQueryMAC(enderecoMAC)
+    MACBD = executarQuery(buscaMACQuery)
+    print(MACBD)
+    if MACBD != enderecoMAC:
+        
+        insertMACQuery = montaQueryMAC(enderecoMAC)
+        executarQuery(insertMACQuery)
+        print(insertMACQuery)
+        
+        buscaIDQuery = buscaQueryIdmaquina(enderecoMAC)
+        MaquinaId = executarQuery(buscaIDQuery)
+        idMaquina = MaquinaId[0][0]
+        print(idMaquina)
+        print('nova máquina inserida')
+        
+        while True:
 
         # Caso o usuário queira, insere no banco de dados aqui:
-        if (habilitarInsercao):
+            if (habilitarInsercao):
 
-             porcentagemCpu = ps.cpu_percent(interval=1)
-             memoria = ps.virtual_memory()
-             porcentagemMemoria = memoria.percent
-             usoDisco = ps.disk_usage('/')
-             #usadoDisco = usoDisco.used / (1024 ** 3)  # Convertendo de bytes para GB
-             livreDisco = usoDisco.free / (1024 ** 3)  # Convertendo de bytes para GB
-             DiscoLivre = round(livreDisco,1)
-             
-             insertCPUQuery = montaQueryCPU(porcentagemCpu)
-             executarQuery(insertCPUQuery)
+                 porcentagemCpu = ps.cpu_percent(interval=1)
+                 memoria = ps.virtual_memory()
+                 porcentagemMemoria = memoria.percent
+                 usoDisco = ps.disk_usage('/')
+                 livreDisco = usoDisco.free / (1024 ** 3)  # Convertendo de bytes para GB
+                 DiscoLivre = round(livreDisco,1)
 
-             insertMEMQuery = montaQueryMEM(porcentagemMemoria)
-             executarQuery(insertMEMQuery)
+                 data = datetime.now()
+                 insertCPUQuery = montaQueryCPU(idMaquina, porcentagemCpu, data)
+                 resultado = executarQuery(insertCPUQuery)
+                 print(resultado)
 
-             insertDISKQuery = montaQueryDISK(DiscoLivre)
-             executarQuery(insertDISKQuery)
-        # Imprime os dados na tabela
-        print(formataLinha(porcentagemCpu, porcentagemMemoria, DiscoLivre)) # Utilizando o "*" como forma de "expandir" a tupla em dados separados, pois se passasse sem isso, ficaria apenas 1 tupla inteira como parâmetro.
-        time.sleep(2)
+                 data = datetime.now()
+                 insertMEMQuery = montaQueryMEM(idMaquina, porcentagemMemoria, data)
+                 resultado = executarQuery(insertMEMQuery)
+                 print(resultado)
 
-def obtemMAC():
-   enderecoMac = ps.AF_LINK
-   return (enderecoMac)
+                 data = datetime.now()
+                 insertDISKQuery = montaQueryDISK(idMaquina, DiscoLivre, data)
+                 resultado = executarQuery(insertDISKQuery)
+                 print(resultado)
+ 
+            # Imprime os dados na tabela
+            print(formataLinha(porcentagemCpu, porcentagemMemoria, DiscoLivre)) # Utilizando o "*" como forma de "expandir" a tupla em dados separados, pois se passasse sem isso, ficaria apenas 1 tupla inteira como parâmetro.
+            time.sleep(5)
 
 
-def montaQueryMAC(*enderecoMAC):
-    return ("INSERT INTO maquina (MACAdress) VALUES ('{}')").format(*enderecoMAC)
+def montaQueryMAC(enderecoMAC):
+    return ("INSERT INTO maquina (MACAdress) VALUES ('{}')").format(enderecoMAC)
 
-def montaQueryCPU(*porcentagemCpu):
-    return (" insert into captura values (null, 1, 1, 1, '{}', null);").format(*porcentagemCpu)
+def montaQueryCPU(idMaquina,porcentagemCpu, data):
+    return (" insert into captura values (null, '{}', 1, 2, '{}', '{}');").format(idMaquina,porcentagemCpu, data)
 
-def montaQueryMEM(*porcentagemMemoria):
-    return ("insert into captura values (null, 1, 1, 1, '{}', null);").format(*porcentagemMemoria)
+def montaQueryMEM(idMaquina,porcentagemMemoria, data):
+    return ("insert into captura values (null, '{}', 3, 4, '{}', '{}');").format(idMaquina,porcentagemMemoria, data)
 
-def montaQueryDISK(*DiscoLivre):
-    return ("insert into captura values (null, 1, 1, 1, '{}', null);").format(*DiscoLivre)
+def montaQueryDISK(idMaquina,DiscoLivre, data):
+    return ("insert into captura values (null, '{}', 5, 1, '{}', '{}');").format(idMaquina,DiscoLivre, data)
+
+def buscaQueryMAC (enderecoMAC):
+    return("select * from maquina where MACAdress = '{}';").format(enderecoMAC)
+
+def buscaQueryIdmaquina (enderecoMAC):
+    return("select idMaquina from maquina where MACAdress = '{}';").format(enderecoMAC)
 
 
 def executarQuery(script): # Função responsável por inserir os dados no banco, recebe uma query SQL qualquer como parâmetro e a executa, usando as credenciais específicas
@@ -72,18 +95,21 @@ def executarQuery(script): # Função responsável por inserir os dados no banco
        db = connect(**config)
        if db.is_connected():
          db_info = db.get_server_info()
-         print('Connected to MySQL server version -', db_info)
 
          with db.cursor() as cursor:
            cursor.execute(script)
+           resultado = cursor.fetchall()
            db.commit()  # Confirma a transação no banco de dados
-         print("Dados inseridos com sucesso!")
+         print("Dados inseridos/buscados com sucesso! \n")
 
          cursor.close()
          db.close()
+         return resultado
+
 
      except Error as e:
        print('Erro ao conectar com o MySQL -', e)
+       return None
 
 def imprimeMenu(texto): # Função responsável por imprimir o menu inicial, contém o nosso nome em ASCII ART, recebe os parâmetros de: texto a ser impresso e tempo a esperar após imprimir
     print(""" 
